412d157ad3ffd8a89a2fe5ffb6e350181e4e4999
Agna 3D Designs Pvt Ltd <agnacloudworks@gmail.com>
2026-06-11 16:31:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Supabase settings
 // Paste Project URL and Publishable Key only. Never paste the Secret Key.
-export const SUPABASE_URL = "https://ivywvrxmtainbaplhohkc.supabase.co";
+export const SUPABASE_URL = "https://iwywrxmtainbaplhohkc.supabase.co/rest/v1/";
 export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_1GXyQ_n3fDtmPQF_EuXb8w_SqCUn0Im";
 
